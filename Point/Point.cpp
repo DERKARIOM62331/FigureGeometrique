b21bf82251eb91591ecdf5b7267f23d11efb5a19
Derkariom <derkariom@localhost.localdomain>
2022-11-17 21:51:06 +0100

    feat (Point) : debut du projet

---
+++ Point/Point.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+#include "Point.hpp"
+using namespace std;
+Point::Point(double x , double y)
+{
+    this->X = x;
+    this->Y = y;
+}
+void Point::Move(double x , double y)
+{
+    this->X += x;
+    this->Y += y;    
+}
+double Point::Distance(Point B)
+{
+   return(sqrt((this->X - B.X)*(this->X - B.X) + (this->Y - B.Y)*(this->Y - B.Y)));
+}
+Point Point::Add(Point B)
+{
+    Point C(0,0);
+    C.X = this->X + B.X;
+    C.Y = this->Y + B.Y;
+    return C;
+}
+void Point::Afficher()
+{
+    cout << "(" << this->X << "," << this->Y << ")" << endl;
+}
+double Point::GetX()
+{
+    return(this->X);
+}
+double Point::GetY()
+{
+    return(this->Y);
+}
+void Point::SetX(double x)
+{
+    this->X = x;
+}
+void Point::SetY(double y)
+{
+    this->Y = y;
+}
---
+++ Point/Point.hpp
@@ -0,0 +1,19 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+    class Point
+    {
+        protected:
+            double X;
+            double Y;
+        public:
+            Point(double,double);
+            void Move(double,double);
+            double Distance(Point);
+            Point Add(Point);
+            void Afficher();
+            double GetX();
+            double GetY();
+            void SetX(double);
+            void SetY(double);
+    };
+#endif
